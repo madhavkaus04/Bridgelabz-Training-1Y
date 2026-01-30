4916716e9afc5b1e278523157775edd6bd81aeb9
Madhav Kaushik <madhavkaus0409@gmail.com>
2026-01-30 10:05:00 +0530

    Names

---
+++ arrays/Names.java
@@ -0,0 +1,18 @@
+class Names {
+    public static void main(String[] args) {
+
+        String[] names = {
+            "Prince",
+            "Anant",
+            "Kartik",
+            "Madhav",
+            "Piyush",
+            "Lukesh"
+        };
+
+        System.out.println("Names:");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
