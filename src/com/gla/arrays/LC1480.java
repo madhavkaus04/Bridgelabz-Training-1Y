cc7f786f4937be9463231d850ada6226d9f4b7fd
Madhav Kaushik <madhavkaus0409@gmail.com>
2026-02-05 09:14:11 +0530

    Madhav completed class work

---
+++ src/com/gla/arrays/LC1480.java
@@ -0,0 +1,16 @@
+package com.gla.arrays;
+
+public class LC1480 {
+    public static int[] runningSum(int[] nums) {
+        int sum=0;
+        int[] nums1 = new int[nums.length];
+        for(int i=0; i<nums.length;i++){
+            sum = sum + nums[i];
+            nums1[i]=sum;
+        }
+        return nums1;
+    }
+    public static void main (String args[]){
+
+    }
+}
